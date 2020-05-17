8c5ece94f3dc275337a87fee1d27e8984ec76158
LordPompax <capitaine_ted@hotmail.fr>
2020-05-17 18:26:56 +0200

    fix : tab -> 4 espace & diagramme de cas

--- Case.java
+++ Case.java
@@ -1,36 +1,36 @@
 public class Case {
-	private int typeId;
-	private int ground;
-	private int build;
-	private String test;
+    private int typeId;
+    private int ground;
+    private int build;
+    private String test;
 
-	public Case() {
-		this.typeId = 0;
-		this.ground = 0;
-		this.build = 0;
-		this.test = "0";
-	}
+    public Case() {
+        this.typeId = 0;
+        this.ground = 0;
+        this.build = 0;
+        this.test = "0";
+    }
 
-	public Case(int e, int g, int b) {
-		this.typeId = e;
-		this.ground = g;
-		this.build = b;
-		this.test = "0";
-	}
+    public Case(int e, int g, int b) {
+        this.typeId = e;
+        this.ground = g;
+        this.build = b;
+        this.test = "0";
+    }
 
-	public int getTypeId() {
-		return this.typeId;
-	}
+    public int getTypeId() {
+        return this.typeId;
+    }
 
-	public int getGround() {
-		return this.ground;
-	}
+    public int getGround() {
+        return this.ground;
+    }
 
-	public int getBuild() {
-		return this.build;
-	}
+    public int getBuild() {
+        return this.build;
+    }
 
-	public String getTest() {
-		return this.test;
-	}
+    public String getTest() {
+        return this.test;
+    }
 }
--- Item.java
+++ Item.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Item {
-	public Item() {
+    public Item() {
 
-	}
+    }
 }
