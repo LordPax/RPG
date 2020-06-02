7e3d581629d14197a370405525f2dbc5d411449c
LordPompax <capitaine_ted@hotmail.fr>
2020-06-02 10:44:22 +0200

    feat : textuel

--- Client.java
+++ Client.java
@@ -1,24 +1,32 @@
 import src.*;
-import src.views.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class Client {
+public class Client extends JPanel{
     private Map map;
-    private RPGWindow win;
+    private TextWindow win;
 
-    public Client(String title) {
-        this.map = new Map(5, 5);
-        this.win = new RPGWindow(title);
+    public Client() {
+        super();
+        this.map = new Map(10, 10);
+        this.win = new TextWindow(this.map);
+        this.addKeyListener(new ListenerKeyBoard());
     }
 
-    public Client(String title, String addr, int port) {}
+    public Client(String addr, int port) {}
 
     public void connect(String addr, int port) {}
 
+    public void showWindow() {
+        System.out.println("putin de test \033[31m azeazeaze");
+    }
+
     public void test() {
         System.out.println("\n================ test map =================\n");
-        this.map.afficheMap();
+        // this.map.afficheMap();
         System.out.println(this.map.getCase(0,0).getTest());
         System.out.println(this.map.getCase(1,0).getTest());
         System.out.println(this.map.getCase(0,1).getTest());
@@ -34,7 +42,8 @@ public class Client {
     }
 
     public static void main(String[] args){
-        Client s = new Client("RPG Client Alpha");
-        s.test();
+        Client c = new Client();
+        c.showWindow();
+        // s.test();
     }
 }
--- src/Map.java
+++ src/Map.java
@@ -2,6 +2,11 @@ package src;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.json.JSONObject;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+// import java.util.*;
 
 public class Map {
     private Case[] matrice;
@@ -14,6 +19,15 @@ public class Map {
     public Map() {}
 
     public Map(int w, int h) {
+        this.player = new ArrayList<Entity>();
+        this.mob = new ArrayList<Entity>();
+        this.pnj = new ArrayList<Entity>();
+        this.w = w;
+        this.h = h;
+        this.initMap();
+    }
+
+    public Map(String file) {
         this.player = new ArrayList<Entity>();
         this.mob = new ArrayList<Entity>();
         this.pnj = new ArrayList<Entity>();
@@ -39,15 +53,49 @@ public class Map {
         return this.matrice;
     }
 
-    public void afficheMap() {
-        String aff = "";
+    public JSONObject readJSON(String file) {
+        String path = "./Save/" + file + ".json";
+        String content = this.charge(path);
+        
+        return new JSONObject(content);
+    }
 
-        for (int i = 0; i < this.h; i++) {
-            for (int j = 0; j < this.w; j++)
-                aff += this.getCase(j, i).getTest();
-            
-            System.out.println(aff);
-            aff = "";
+    public String charge(String file) {
+        String line, content = "";
+        try {
+            BufferedReader br = Files.newBufferedReader(Paths.get(file));
+
+            while ((line = br.readLine()) != null)
+                content += line + "\n";
+ 
+            br.close();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+
+        return content;
+    }
+
+    public void save(String nameFile, String content) {
+        try {
+            BufferedWriter bw = Files.newBufferedWriter(Paths.get(nameFile));
+
+            bw.write(content);
+            bw.close();
+        } catch (IOException e) {
+            System.err.println(e);
         }
     }
+
+    // public void afficheMap() {
+    //     String aff = "";
+
+    //     for (int i = 0; i < this.h; i++) {
+    //         for (int j = 0; j < this.w; j++)
+    //             aff += this.getCase(j, i).getTest();
+            
+    //         System.out.println(aff);
+    //         aff = "";
+    //     }
+    // }
 }
